55e47ee528f5aa25daa93686b8418bd917aeb5c7
Jhonatan <jahv2016@hotmail.com>
2023-05-14 17:09:37 -0500

    Se adjuntan los archivos

---
+++ interactivity.js
@@ -0,0 +1,10 @@
+//five rounds
+var rock = document.querySelector(".rock");
+var sheet = document.querySelector(".sheet");
+var scissor = document.querySelector(".scissors");
+var score_player = document.querySelector(".player_score");
+var score_cpu = document.querySelector(".cpu_score");
+
+rock.addEventListener("click", function(){alert("hola")});
+
+score_cpu.textContent = "0";
