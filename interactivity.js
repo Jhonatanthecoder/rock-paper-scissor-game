26f4d98f6fe714c693df059fb80cc8df659765c0
Jhonatan <jahv2016@hotmail.com>
2023-05-14 17:48:24 -0500

    modificacion al archivo js

--- interactivity.js
+++ interactivity.js
@@ -4,7 +4,4 @@ var sheet = document.querySelector(".sheet");
 var scissor = document.querySelector(".scissors");
 var score_player = document.querySelector(".player_score");
 var score_cpu = document.querySelector(".cpu_score");
-
-rock.addEventListener("click", function(){alert("hola")});
-
-score_cpu.textContent = "0";
+var winner = document.querySelector(".result");
