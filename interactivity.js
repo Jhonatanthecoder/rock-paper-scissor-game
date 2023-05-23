e84a4ab53ef46502ae4d4948f40a32502864c881
Jhonatan <jahv2016@hotmail.com>
2023-05-22 20:12:48 -0500

    se modifica interactivity.js

--- interactivity.js
+++ interactivity.js
@@ -5,3 +5,4 @@ var scissor = document.querySelector(".scissors");
 var score_player = document.querySelector(".player_score");
 var score_cpu = document.querySelector(".cpu_score");
 var winner = document.querySelector(".result");
+
